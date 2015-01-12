68963109954552e6bee1843421c4e0c5ad166515
paulschreiber <paulschreiber@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-12 04:51:23 +0000

    Code style fixes from PHPCBF. updated to 0.2.2.
    git-svn-id: https://plugins.svn.wordpress.org/get-latest-post/trunk@1065609 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- get-latest-post.php
+++ get-latest-post.php
@@ -3,28 +3,28 @@
 Plugin Name: Get Latest Post
 Plugin URI: http://paulschreiber.com/blog/2011/02/04/how-to-display-the-latest-blog-post-on-a-wordpress-page/
 Description: Adds a shortcode tag [get_latest_post] to display an excerpt from the latest post
-Version: 0.2.1
+Version: 0.2.2
 Author: Paul Schreiber
 Author URI: http://paulschreiber.com/
 */
 
-/*  Copyright 2011 Paul Schreiber <paul at paulschreiber.com>
+/*  Copyright 2011-15 Paul Schreiber <paul at paulschreiber.com>
 
     Released under the GPL, version 2.
 */
 
 function get_latest_post_html() {
-	$content = "";
-	query_posts('showposts=1');
-	while (have_posts()){
+	$content = '';
+	query_posts( 'showposts=1' );
+	while ( have_posts() ) {
 		the_post();
 		$content .= "<p class='title'><a href='" . get_permalink() . "'>" . get_the_title() . "</a></p>\n" .
-				"<p class='excerpt'>" . get_the_excerpt() . "</p>";
+				"<p class='excerpt'>" . get_the_excerpt() . '</p>';
 	}
 	wp_reset_query();
 
 	return "<div class='latest-post'>\n$content\n</div>";
 }
 
-add_shortcode('get_latest_post', 'get_latest_post_html');
+add_shortcode( 'get_latest_post', 'get_latest_post_html' );
 ?>
